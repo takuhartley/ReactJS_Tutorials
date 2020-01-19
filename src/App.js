a04a532c0cce0c6f0d678fa29112d118e240fe68
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-20 07:41:16 +0900

    Understanding and Using State

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import './App.css';
-import Person from './Person/Person';
-function App() {
-  return (
-    <div className="App">
-      <p>App</p>
-      <Person name="Cameron" age="24">My Hobbies: Gaming</Person>
-      <Person name="Robert" age="22">My Hobbies: Coding</Person>
-      <Person name="Alex" age="21">My Hobbies: Modeling</Person>
-      <Person name="Clifford" age="19">My Hobbies: Smoking</Person>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
+class App extends Component {
+  // Managed from inside component
+  state = {
+    persons: [
+      { name: "Cameron", age: 24 },
+      { name: "Robert", age: 22 },
+      { name: "Alexander", age: 21 },
+      { name: "Clifford", age: 19 }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>App</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>
+          My Hobbies: Gaming
+        </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+          My Hobbies: Coding
+        </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+          My Hobbies: Modeling
+        </Person>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}>
+          My Hobbies: Smoking
+        </Person>
+      </div>
+    );
+  }
 }
 
 export default App;
