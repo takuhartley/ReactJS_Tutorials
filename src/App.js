89030923583448dadb019d1dbe606639ecc6ece8
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-20 07:24:50 +0900

    Outputting Dynamic Content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import Person from './Person/Person';
 function App() {
   return (
     <div className="App">
       <p>App</p>
+      <Person />
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,3 +1,7 @@
-const person = () => {
-    
-}
+import React from 'react';
+
+const Person = () => {
+    return <p>I'm a Person and I'm {Math.floor(Math.random() * 30)} years old.</p>
+};
+
+export default Person;
