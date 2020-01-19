224fad37145a3d6308c8b78477f9d766a5c5b0db
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-20 07:49:17 +0900

    Manipulating State

--- src/App.js
+++ src/App.js
@@ -9,29 +9,50 @@ class App extends Component {
       { name: "Robert", age: 22 },
       { name: "Alexander", age: 21 },
       { name: "Clifford", age: 19 }
-    ]
-  }
+    ],
+    otherState: "some other value"
+  };
 
   //Switch Name
   switchNameHandler = () => {
-    console.log('Was clicked!');
-  }
+    //console.log('Was clicked!');
+    this.setState({
+      persons: [
+        { name: "Cam", age: 24 },
+        { name: "Rob", age: 22 },
+        { name: "Alex", age: 21 },
+        { name: "Cliff", age: 19 }
+      ]
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <p>App</p>
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
           My Hobbies: Gaming
         </Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           My Hobbies: Coding
         </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        >
           My Hobbies: Modeling
         </Person>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}>
+        <Person
+          name={this.state.persons[3].name}
+          age={this.state.persons[3].age}
+        >
           My Hobbies: Smoking
         </Person>
       </div>
