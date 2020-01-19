3858033affc7ad69348fd8fb9bd8180883bfa9b4
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-20 07:31:58 +0900

    Understanding Children Property

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <p>App</p>
-      <Person name="Cameron" age="24" />
+      <Person name="Cameron" age="24">My Hobbies: Gaming</Person>
       <Person name="Robert" age="22">My Hobbies: Coding</Person>
-      <Person name="Alex" age="21" />
-      <Person name="Clifford" age="19" />
+      <Person name="Alex" age="21">My Hobbies: Modeling</Person>
+      <Person name="Clifford" age="19">My Hobbies: Smoking</Person>
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const Person = (props) => {
-    return <p>I'm a {props.name} and I'm {props.age} years old.</p>
+const Person = props => {
+  return (
+    <div>
+      <p>
+        I'm a {props.name} and I'm {props.age} years old.
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
 };
 
 export default Person;
