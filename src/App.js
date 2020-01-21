a093e55d23ad936d31a04057666499c881a63c83
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-21 19:56:31 +0900

    Updating React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 import "./App.css";
 
-const App = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: "Cameron", age: 24 },
       { name: "Robert", age: 22 },
@@ -12,24 +12,22 @@ const App = props => {
     ],
     otherState: "some other value",
     showPersons: true
-  });
-  const [otherState, setOtherState] = useState("some other value");
-  console.log(personsState, otherState);
+  };
 
-  const switchNameHandler = newName => {
+  switchNameHandler = newName => {
     //console.log('Was clicked!');
-    setPersonsState({
+    this.setState({
       persons: [
         { name: newName, age: 24 },
         { name: "Rob", age: 22 },
         { name: "Alex", age: 21 },
         { name: "Cliff", age: 19 }
       ],
-      otherState: setOtherState
+      otherState: this.otherState
     });
   };
-  const nameChangeHandler = event => {
-    setPersonsState({
+  nameChangeHandler = event => {
+    this.setState({
       persons: [
         { name: "Cameron", age: 24 },
         { name: event.target.value, age: 22 },
@@ -38,65 +36,43 @@ const App = props => {
       ]
     });
   };
-
-  const togglePersonsHandler = () => {
-    const doesShow = personsState.showPersons;
-    // console.log(personsState.showPersons);
-    // console.log(doesShow);
-    setPersonsState({ personsState: !doesShow });
+  
+  togglePersonsHandler = () => {
+    const doesShow = this.showPersons;
+    this.setState({ showPersons: !doesShow });
   };
 
-  const style = {
-    backgroundColor: "white",
-    font: "inherit",
-    border: "1px solid blue",
-    padding: "8px",
-    cursor: "pointer"
-  };
+  render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
 
-  let persons = null;
+    let persons = null;
 
-  if(personsState.showPersons) {
-    persons = (
-      <div>
-          <Person
-            name={personsState.persons[0].name}
-            age={personsState.persons[0].age}
-          >
-            My Hobbies: Gaming
-          </Person>
-          <Person
-            name={personsState.persons[1].name}
-            age={personsState.persons[1].age}
-            click={switchNameHandler.bind(setPersonsState, "Fuck Boi")}
-            chaged={togglePersonsHandler}
-          >
-            My Hobbies: Coding
-          </Person>
-          <Person
-            name={personsState.persons[2].name}
-            age={personsState.persons[2].age}
-          >
-            My Hobbies: Modeling
-          </Person>
-          <Person
-            name={personsState.persons[3].name}
-            age={personsState.persons[3].age}
-          >
-            My Hobbies: Smoking
-          </Person>
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.personsState.map(person => {
+            return <Person name={person.name} age={person.age} />;
+          })}
         </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <p>App</p>
+        <button style={style} onClick={this.nameChangeHandler}>
+          Switch Name
+        </button>
+        {persons}
+      </div>
     );
   }
-  return (
-    <div className="App">
-      <p>App</p>
-      <button style={style} onClick={nameChangeHandler}>
-        Switch Name
-      </button>
-      {persons}
-    </div>
-  );
-};
+}
 
 export default App;
